ddcfdf79831ceba2b916ab2fcaac65e4373b40fb
nasty8008 <nastya901296@gmail.com>
2026-01-21 19:36:10 +0400

    KTB-04 Initial Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,14 +2,25 @@ package org.example
 
 import java.io.File
 
+data class Word(
+    val original: String,
+    val translation: String,
+    var correctAnswersCount: Int? = 0
+)
+
 fun main() {
     val wordsFile = File("words.txt")
-    wordsFile.writeText(
-        "hello привет\n" +
-                "dog собака\n" +
-                "cat кошка"
-    )
-    wordsFile.readLines().forEach {
-        println(it)
+    val dictionary = mutableListOf<Word>()
+    wordsFile.readLines().forEach { line: String ->
+        val line = line.split("|")
+        val word = Word(
+            original = line[0],
+            translation = line[1],
+            correctAnswersCount = line.getOrNull(2)?.toIntOrNull() ?: 0
+        )
+        dictionary.add(word)
+    }
+    dictionary.forEach { word ->
+        println(word)
     }
 }
