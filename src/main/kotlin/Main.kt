e0888eceadb090cb7d467592c5040f229af2202c
nasty8008 <nastya901296@gmail.com>
2026-01-22 13:55:36 +0400

    KTB-04 Переименовала переменную line в parts и заменила тип переменной correctAnswersCount на Int + запушила файл words.txt (забыла про него в прошлом коммите)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,18 +5,18 @@ import java.io.File
 data class Word(
     val original: String,
     val translation: String,
-    var correctAnswersCount: Int? = 0
+    var correctAnswersCount: Int = 0
 )
 
 fun main() {
     val wordsFile = File("words.txt")
     val dictionary = mutableListOf<Word>()
     wordsFile.readLines().forEach { line: String ->
-        val line = line.split("|")
+        val parts = line.split("|")
         val word = Word(
-            original = line[0],
-            translation = line[1],
-            correctAnswersCount = line.getOrNull(2)?.toIntOrNull() ?: 0
+            original = parts[0],
+            translation = parts[1],
+            correctAnswersCount = parts.getOrNull(2)?.toIntOrNull() ?: 0
         )
         dictionary.add(word)
     }
