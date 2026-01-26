e1156034aade5f07abcc44b5df63833d769e4465
nasty8008 <nastya901296@gmail.com>
2026-01-26 23:10:05 +0400

    KTB-08 Избавилась от магических чисел, перенесла расчет статистики в блок case 2, вынесла функцию saveDictionary() на верхний уровень и изменила цикл в блоке case 1 так, чтобы слова "запоминались" как выученные

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,24 +2,26 @@ package org.example
 
 import java.io.File
 
+const val CORRECT_ANSWERS = 3
+const val MAX_OPTIONS = 4
+
 data class Word(
     val original: String,
     val translation: String,
-    var correctAnswersCount: Int = 0
+    var correctAnswersCount: Int = 0,
 )
 
 fun main() {
     val wordsFile = File("words.txt")
     val dictionary = loadDictionary(wordsFile)
-    val totalCount = dictionary.size
-    val learnedCount = dictionary.filter {it.correctAnswersCount >= 3}.size
-    val percent = if (totalCount == 0) "Словарь пуст!" else "${100 * learnedCount / totalCount}%"
 
     while (true) {
-        println("Меню: \n" +
-                "1 – Учить слова\n" +
-                "2 – Статистика\n" +
-                "0 – Выход")
+        println(
+            "Меню: \n" +
+                    "1 – Учить слова\n" +
+                    "2 – Статистика\n" +
+                    "0 – Выход"
+        )
         print("Введите номер пункта меню: ")
         val choice: Int? = readln().toIntOrNull()
         when (choice) {
@@ -27,61 +29,67 @@ fun main() {
                 println("Выходим из программы...")
                 return
             }
+
             1 -> {
                 println("Выбран пункт \"Учить слова\"")
-
-                val notLearnedList = dictionary.filter { it.correctAnswersCount < 3}
-
-                while (notLearnedList.isNotEmpty()) {
-                    val questionWords: List<Word> = if (notLearnedList.size < 4) {
-                        notLearnedList.shuffled().take(minOf(4, notLearnedList.size))
+                while (true) {
+                    val notLearnedList = dictionary.filter { it.correctAnswersCount < CORRECT_ANSWERS }
+                    val questionWords: List<Word> = if (notLearnedList.size < MAX_OPTIONS) {
+                        notLearnedList.shuffled().take(minOf(MAX_OPTIONS, notLearnedList.size))
                     } else {
-                        notLearnedList.shuffled().take(4)
+                        notLearnedList.shuffled().take(MAX_OPTIONS)
                     }
-                    val correctAnswer = questionWords.random()
 
-                    println("${correctAnswer.original}:")
-                    questionWords.forEachIndexed { index, word ->
-                        println(" ${index+1} - ${word.translation}")
-                    }
-                    println(" ----------\n" +
-                            " 0 - Меню")
-                    print("Введите число от 0 до ${questionWords.size}: ")
-                    val userAnswerInput = readln().toIntOrNull()
-                    val correctAnswerId = questionWords.indexOf(correctAnswer)
+                    if (notLearnedList.isNotEmpty()) {
+                        val correctAnswer = questionWords.random()
 
-                    when (userAnswerInput) {
-                        null -> println("Пожалуйста, введите число.")
-                        0 -> break
-                        in 1..questionWords.size -> if (userAnswerInput == correctAnswerId + 1) {
-                            println("Правильно!")
-                            dictionary[dictionary.indexOf(correctAnswer)].correctAnswersCount++
+                        println("${correctAnswer.original}:")
+                        questionWords.forEachIndexed { index, word ->
+                            println(" ${index + 1} - ${word.translation}")
+                        }
+                        println(
+                            " ----------\n" +
+                                    " 0 - Меню"
+                        )
+                        print("Введите число от 0 до ${questionWords.size}: ")
+                        val userAnswerInput = readln().toIntOrNull()
+                        val correctAnswerId = questionWords.indexOf(correctAnswer)
 
-                            fun saveDictionary(dictionary: MutableList<Word>) {
-                                wordsFile.writeText("")
-                                dictionary.forEach {
-                                    wordsFile.appendText("${it.original}|${it.translation}|${it.correctAnswersCount}\n")
-                                }
+                        when (userAnswerInput) {
+                            null -> println("Пожалуйста, введите число.")
+                            0 -> break
+                            in 1..questionWords.size -> if (userAnswerInput == correctAnswerId + 1) {
+                                println("Правильно!")
+                                dictionary[dictionary.indexOf(correctAnswer)].correctAnswersCount++
+                                saveDictionary(dictionary, wordsFile)
+                            } else {
+                                println("Неправильно! ${correctAnswer.original} - это ${correctAnswer.translation}")
                             }
 
-                            saveDictionary(dictionary)
-                        } else {
-                            println("Неправильно! ${correctAnswer.original} - это ${correctAnswer.translation}")
+                            else -> continue
                         }
-                        else -> continue
+                    } else {
+                        println("Все слова в словаре выучены!")
+                        break
                     }
                 }
-                println("Все слова в словаре выучены!")
             }
+
             2 -> {
+                val totalCount = dictionary.size
+                val learnedCount = dictionary.filter { it.correctAnswersCount >= CORRECT_ANSWERS }.size
+                val percent = if (totalCount == 0) "Словарь пуст!" else "${100 * learnedCount / totalCount}%"
                 println("Выбран пункт \"Статистика\"")
-                println("""
+                println(
+                    """
                     Всего слов в словаре: $totalCount
                     Выучено слов: $learnedCount
                     Выучено $learnedCount из $totalCount слов | $percent
                     
-                """.trimIndent())
+                """.trimIndent()
+                )
             }
+
             else -> println("Введите число 1, 2 или 0")
         }
     }
@@ -99,4 +107,11 @@ fun loadDictionary(file: File): MutableList<Word> {
         dictionary.add(word)
     }
     return dictionary
+}
+
+fun saveDictionary(dictionary: MutableList<Word>, wordsFile: File) {
+    wordsFile.writeText("")
+    dictionary.forEach {
+        wordsFile.appendText("${it.original}|${it.translation}|${it.correctAnswersCount}\n")
+    }
 }
