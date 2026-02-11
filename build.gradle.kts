8e0b65033f222d7e8f60abcfa554d974dd73ee84
nasty8008 <nastya901296@gmail.com>
2026-02-11 12:59:21 +0400

    fix entry point and jdk version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "2.3.10"
     kotlin("plugin.serialization") version "2.3.10"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    application
 }
 
 group = "org.example"
@@ -18,6 +20,11 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
-    jvmToolchain(24)
+    jvmToolchain(21)
+}
+
+application {
+    mainClass.set("org.example.TelegramKt")
 }
